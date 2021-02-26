7086bd1ae12bfcd821ad89424cb7ca716ab3a698
reallithuanianmafia <reallithuanianmafia@outlook.com>
2021-02-26 22:57:26 +0400

    no message

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  mounted()
+  {
+    const axios = require('axios');
+    axios.get('//api.example.com')
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  },
+}
+</script>
